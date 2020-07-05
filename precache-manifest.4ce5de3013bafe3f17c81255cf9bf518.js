f13046ec5034597bbc102eceb186596338a3d078
Maor Cohen <maorc80@gmail.com>
2020-07-05 15:00:27 +0300

    Updates

---
+++ precache-manifest.4ce5de3013bafe3f17c81255cf9bf518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993177e00cf0dea6469c869fa5e3ef9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17974a57a91a59a6de23",
+    "url": "/static/js/2.b21bb5ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b21bb5ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec28086c6147f0ea530d",
+    "url": "/static/js/main.7f4255f5.chunk.js"
+  },
+  {
+    "revision": "9c63f1ba4335c0661a65",
+    "url": "/static/js/runtime-main.bfb6961d.js"
+  }
+]);
