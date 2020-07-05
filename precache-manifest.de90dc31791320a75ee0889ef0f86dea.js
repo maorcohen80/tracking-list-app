21e168b668383cc432e23335066cf5e870cb97b5
Maor Cohen <maorc80@gmail.com>
2020-07-05 15:06:55 +0300

    Updates

---
+++ precache-manifest.de90dc31791320a75ee0889ef0f86dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db52be6647ec6be6ba49a2354102e0bc",
+    "url": "/tracking-list-app/index.html"
+  },
+  {
+    "revision": "17974a57a91a59a6de23",
+    "url": "/tracking-list-app/static/js/2.b21bb5ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tracking-list-app/static/js/2.b21bb5ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec28086c6147f0ea530d",
+    "url": "/tracking-list-app/static/js/main.7f4255f5.chunk.js"
+  },
+  {
+    "revision": "8a57407c2eb71dca7d8b",
+    "url": "/tracking-list-app/static/js/runtime-main.9d1eeb9e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ce5de3013bafe3f17c81255cf9bf518.js"
+  "/tracking-list-app/precache-manifest.de90dc31791320a75ee0889ef0f86dea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tracking-list-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
